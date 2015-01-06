6b98d3e2d99c2cbb918e6288d353b8b563217128
Steven Das <steven@zirtual.com>
2015-01-05 23:20:33 -0600

    First attempt at a method to return the config info.

--- test_app.py
+++ test_app.py
@@ -8,3 +8,12 @@ INIConfig(app)
 
 with app.app_context():
   app.config.from_inifile('settings.ini')
+
+
+@app.route('/')
+def return_config():
+  return app.config
+
+
+if __name__ == '__main__':
+  app.run()
