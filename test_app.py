b2fa4c4e0a3b46ae7edc3a3b779fb099938aa4a2
Steven Das <steven@zirtual.com>
2015-01-06 23:26:20 -0600

    connect_db method.

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.iniconfig import INIConfig
+import sqlite3
 
 
 app = Flask(__name__)
@@ -9,12 +10,8 @@ INIConfig(app)
 with app.app_context():
   app.config.from_inifile('settings.ini')
 
-
-@app.route('/')
-def return_config():
-  print('Config: %s' % app.config)
-  return str(app.config)
-
+def connect_db():
+  return sqlite3.connect(app.config['DATABASE'])
 
 if __name__ == '__main__':
   app.run()
