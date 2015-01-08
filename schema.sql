5416ee0d2dbd8f1b85eca6e33ed3b164bd07832e
Steven Das <steven@zirtual.com>
2015-01-07 22:53:05 -0600

    Removed comma from SQL producing syntax error.

--- schema.sql
+++ schema.sql
@@ -4,5 +4,5 @@ create table locations (
   id integer primary key autoincrement,
   latitude real not null,
   longitude real not null,
-  timestamp integer not null,
+  timestamp integer not null
 );
