9e20dd27ddf1fef4d7fb45cb60c1a0d0abf1ff66
Steven Das <steven@zirtual.com>
2015-01-08 00:23:43 -0600

    init_db method.

--- test_app.py
+++ test_app.py
@@ -15,5 +15,11 @@ with app.app_context():
 def connect_db():
   return sqlite3.connect(app.config['database']['DATABASE'])
 
+def init_db():
+  with closing(connect_db()) as db:
+    with app.open_resource('schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
+
 if __name__ == '__main__':
   app.run()
